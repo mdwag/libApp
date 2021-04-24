db5d336328be14374e7cf08f0ba6ede0db5b20a2
Matt Johnsen <johnsenwriting.gmail.com>
2021-04-24 18:45:58 -0500

    added the form as a popup to save space

--- lib.js
+++ lib.js
@@ -7,32 +7,47 @@ let authorInput = document.querySelector('#author-input');
 let pagesInput = document.querySelector('#pages-input');
 let subBtn = document.querySelector('#btn');
 let haveReadBox = document.querySelector('#have-read');
-let radio = document.getElementsByName("readYet");
-let readYet = false;
+let readBox = document.querySelector("#read-yet");
+let newBookBtn = document.querySelector("#new-book");
+let popUpForm = document.querySelector("#pop-up");
 
 
 //put the event listener on the submit button
 subBtn.addEventListener("click", addBookToLibrary);
+newBookBtn.addEventListener("click", showForm);
 
+
+function showForm(){
+  newBookBtn.style.visibility = "hidden";
+  newBookBtn.style.display = "none";
+ popUpForm.style.visibility = "visible";
+ popUpForm.style.display = "flex";
+}
+function hideForm(){
+  newBookBtn.style.visibility = "visible";
+  newBookBtn.style.display = "block";
+  popUpForm.style.visibility = "hidden";
+ popUpForm.style.display = "none";
+}
 //constructor function that creates new obects
 function Book(title, author, pages) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.info = `This book is called ${title}. It is by ${author}. It is ${pages} pages long.`;
+    if (readBox.checked === true){
+      this.haveRead = "Yes.";
+    }
+    else if (readBox.checked !== true){
+      this.haveRead = "No";
+    }
 }
-Book.prototype.getInfo = function(){
-    alert(this.info);
-}
-let matt = new Book("matt", "addie", 23);
-
-
-
 
 
 function addBookToLibrary() {
   let bookBoi = new Book(titleInput.value, authorInput.value, pagesInput.value);
   myLibrary.unshift(bookBoi);
+  hideForm();
   makeList();
 }
 
@@ -50,18 +65,22 @@ function makeList(){
     let titleDiv = document.createElement("div");
     let authorDiv = document.createElement("div");
     let lengthDiv = document.createElement("div");
+    let readDiv = document.createElement("div");
 
     let titleText = document.createTextNode(`Title: ${myLibrary[i].title}`);
     let authorText = document.createTextNode(`Author: ${myLibrary[i].author}`);
     let lengthText = document.createTextNode(`Length: ${myLibrary[i].pages} pages`);
+    let readText = document.createTextNode(`Finished: ${myLibrary[i].haveRead}`); 
 
     titleDiv.appendChild(titleText);
     authorDiv.appendChild(authorText);
     lengthDiv.appendChild(lengthText);
+    readDiv.appendChild(readText);
 
     newCard.appendChild(titleDiv);
     newCard.appendChild(authorDiv);
     newCard.appendChild(lengthDiv);
+    newCard.appendChild(readDiv);
     bookshelf.appendChild(newCard);
     newCard.appendChild(delBtn);
   }
