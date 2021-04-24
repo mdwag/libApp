7f61e6eec13763f5037e2368daf62ba26a5df094
Matt Johnsen <johnsenwriting.gmail.com>
2021-04-23 19:22:45 -0500

    did some stuff

--- lib.js
+++ lib.js
@@ -2,6 +2,10 @@ function Book(title, author, pages) {
     this.title = title,
     this.author = author,
     this.pages = pages;
-    this.getInfo = function() { console.log("This book is " + title + " by " + author + ". It is " + pages + " long.")};
+    this.info = "This book is " + title + " by " + author + ". It is " + pages + " long.";
 }
+Book.prototype.getInfo = function(){
+    alert(this.info);
+}
+
 const matt = new Book("Matt", "addie", 300);
